79165812a8e899235ea9873e072bdd09b52e131c
Methasit DreamX <methasit.dreamx@gmail.com>
2023-05-22 14:20:43 +0700

    add search button

--- src/pages/Shop/shop.jsx
+++ src/pages/Shop/shop.jsx
@@ -5,62 +5,95 @@ import "./shop.css";
 
 export const Shop = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleCategoryFilter = (category) => {
     setSelectedCategory(category);
+    setSearchQuery("");
   };
 
-  const filteredProducts = selectedCategory
-    ? PRODUCTS.filter((product) => product.category === selectedCategory)
-    : PRODUCTS;
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredProducts = PRODUCTS.filter((product) => {
+    const matchesCategory =
+      !selectedCategory || product.category === selectedCategory;
+    const matchesSearch = product.productName
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase());
+    return matchesCategory && matchesSearch;
+  });
   return (
     <div className="shop">
       <div className="shopTitle"></div>
+      <div className="searchBar">
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+          placeholder="ค้นหา"
+        />
+      </div>
       <div className="categoryselect">
         <button
-          className="categoryBTN"
+          className={`categoryBTN ${selectedCategory === null ? "active" : ""}`}
           onClick={() => handleCategoryFilter(null)}
         >
           สินค้าทั้งหมด
         </button>
         <button
-          className="categoryBTN"
+          className={`categoryBTN ${
+            selectedCategory === "snack" ? "active" : ""
+          }`}
           onClick={() => handleCategoryFilter("snack")}
         >
           อาหารรองท้อง ทานเล่น
         </button>
         <button
-          className="categoryBTN"
+          className={`categoryBTN ${
+            selectedCategory === "bakery" ? "active" : ""
+          }`}
           onClick={() => handleCategoryFilter("bakery")}
         >
           เบเกอรี่
         </button>
         <button
-          className="categoryBTN"
+          className={`categoryBTN ${
+            selectedCategory === "ready to eat food" ? "active" : ""
+          }`}
           onClick={() => handleCategoryFilter("ready to eat food")}
         >
           อาหารพร้อมทาน
         </button>
         <button
-          className="categoryBTN"
+          className={`categoryBTN ${
+            selectedCategory === "dessert" ? "active" : ""
+          }`}
           onClick={() => handleCategoryFilter("dessert")}
         >
           ของหวาน
         </button>
         <button
-          className="categoryBTN"
+          className={`categoryBTN ${
+            selectedCategory === "beverage" ? "active" : ""
+          }`}
           onClick={() => handleCategoryFilter("beverage")}
         >
           เครื่องดื่ม
         </button>
         <button
-          className="categoryBTN"
+          className={`categoryBTN ${
+            selectedCategory === "all cafe" ? "active" : ""
+          }`}
           onClick={() => handleCategoryFilter("all cafe")}
         >
           All Cafe
         </button>
         <button
-          className="categoryBTN"
+          className={`categoryBTN ${
+            selectedCategory === "drug" ? "active" : ""
+          }`}
           onClick={() => handleCategoryFilter("drug")}
         >
           ยาประจำบ้าน
